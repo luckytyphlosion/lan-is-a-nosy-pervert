927c05a3a335d1bd500300e2eaa932e924e75a12
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2020-06-02 14:54:06 -0400

    More script command fixes.
    Script commands are actually parsed, but design decision in armips means enum doesn't work.

--- include/bytecode/fix_bn6f_script_macros.py
+++ include/bytecode/fix_bn6f_script_macros.py
@@ -1,6 +1,6 @@
 
-def main():
-    with open("map_script.inc", "r") as f:
+def fix_file(filename):
+    with open(filename, "r") as f:
         output = ""
         for line in f:
             if line.startswith("\t.macro"):
@@ -8,12 +8,17 @@ def main():
                 if len(split_line) == 2:
                     output += line
                 else:
-                    output += f"\t.macro {', '.join(split_line[1:])}\r\n"
+                    output += f"\t.macro {', '.join(split_line[1:])}\n"
             else:
                 output += line
 
-    with open("map_script_out.inc", "w+") as f:
+    with open(filename, "w+") as f:
         f.write(output)
+
+def main():
+    fix_file("cutscene_script.inc")
+    fix_file("cutscene_camera_script.inc")
+    fix_file("npc_script.inc")
 
 if __name__ == "__main__":
     main()
